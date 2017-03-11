9acc727842ba930d075a3147bc6ccc454452c5ab
Valery <xeonv@mail.ru>
2017-03-11 17:18:16 +0400

    create database on app init fix error

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ require 'sqlite3'
 
 configure do
 	@db = SQLite3::Database.new 'BarberShop.sqlite'
-	@db.execute 'CREATE TABLE IF NOT EXIST 
+	@db.execute 'CREATE TABLE IF NOT EXISTS 
 			"Users" 
 			(
-				"Id" INTEGER PRIMARY KEY AUTUINCREMENT, 
+				"Id" INTEGER PRIMARY KEY AUTOINCREMENT, 
 				"Name" TEXT, 
 				"Phone" TEXT, 
 				"DataStamp" TEXT, 
